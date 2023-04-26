642d3264e7b1bbd74123509a55156a20f79a1785
codestrelaoffice <117630546+codestrelaoffice@users.noreply.github.com>
2023-04-27 04:02:03 +0530

    Initial commit

---
+++ src/app/icons/font-awesome/font-awesome.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-font-awesome',
+  templateUrl: './font-awesome.component.html',
+  styleUrls: ['./font-awesome.component.scss'],
+})
+export class FontAwesomeComponent {
+  constructor() {
+    //constructor
+  }
+}
